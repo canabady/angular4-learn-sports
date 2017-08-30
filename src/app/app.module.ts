e551a721269e0b93f12943b070bb72bf9511a142
SelvaCanabady P <canabady@gmail.com>
2017-08-30 12:48:25 +0530

    Header Menu with Output EventEmitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { TeamComponent } from './team/team.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TeamComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
